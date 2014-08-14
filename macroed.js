c3f793f3ca64517399cbdd6c5a048f874c5e1322
golyshevd <golyshev.dmitry@yandex.ru>
2014-08-14 04:35:30 +0400

    Support markdown by default

--- macroed.js
+++ macroed.js
@@ -1,7 +1,25 @@
 'use strict';
 
 var Macroed = require('./core/macroed');
+
+var marked = require('marked');
 var macroed = new Macroed();
+
+macroed.setProcessor({
+    name: 'default',
+    params: {
+        gfm: true,
+        breaks: true,
+        pedantic: false,
+        sanitize: true,
+        smartLists: true,
+        smartypants: true
+    },
+    process: function (params, content) {
+
+        return marked(content, this.params);
+    }
+});
 
 macroed.Macroed = Macroed;
 
